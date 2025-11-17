7886703ead700caa11554298d81d167fa0fc83c8
naveenraj44125-creator <naveenraj44125@gmail.com>
2025-11-17 09:54:16 -0800

    Fix Python syntax warning: escape backslashes in regex patterns

--- workflows/deploy-post-steps-generic.py
+++ workflows/deploy-post-steps-generic.py
@@ -503,12 +503,12 @@ server {{
         try_files $uri $uri/ /index.php?$query_string;
     }}
     
-    location ~ \.php$ {{
+    location ~ \\.php$ {{
         include snippets/fastcgi-php.conf;
         fastcgi_pass unix:/var/run/php/php8.1-fpm.sock;
     }}
     
-    location ~ /\.ht {{
+    location ~ /\\.ht {{
         deny all;
     }}
     
